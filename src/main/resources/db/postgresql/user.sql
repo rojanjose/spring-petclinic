ee02f76878aa865b4babacdb6821d2caf7e60c18
rojanjose <rojanjose@gmail.com>
2020-08-29 17:30:31 -0400

    Add postgresql support

---
+++ src/main/resources/db/postgresql/user.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS petclinic;
+
+--ALTER DATABASE petclinic 
+--  DEFAULT CHARACTER SET utf8
+--  DEFAULT COLLATE utf8_general_ci;
+
+CREATE USER roach;
+
+--CREATE USER roach WITH PASSWORD 'petclinic';
+
+--GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic@%' IDENTIFIED BY 'petclinic';
